d741bc4f7e58a91d94ff2f77695f80858b2d13f1
sumanta <sumanta>
2022-09-13 17:29:55 +0530

    Added new functionalities

--- trial-2.py
+++ trial-2.py
@@ -26,8 +26,17 @@ mainWindow.geometry("600x600")
 resultWindow = Toplevel(mainWindow)
 resultWindow.title("Log Window")
 resultWindow.geometry("600x600")
-logTable = Text(resultWindow, width=500, height=400)
-logTable.pack()
+logTable = Text(resultWindow, width=70, height=30)
+logTable.grid(row=0, column=0, padx=10, pady=10)
+
+def saveResults():
+    global logTable
+    value=(logTable.get("1.0","end-1c"))
+    resultFile = open("result.txt", "w")
+    resultFile.write(value)
+    resultFile.close
+    
+saveButton = Button(resultWindow, text="Save results", command=saveResults, padx=5, pady=5).grid(row=7, column=0, padx=10, pady=10, ipadx=5, ipady=5)
 
 #initializing the main frame
 mainFrame = ttk.Frame(mainWindow, padding = "3 3 12 12")
@@ -41,28 +50,35 @@ proc_Complete = False
 lineNum = 1.0
 filename=""
 
+classifier_models = ["SV Classifier", "KNN Classifier", "Decision Tree Classifier", "Random Forest Classifier"]
+regression_models = ["Decision Tree Regressor", "Random Forest Regressor", "KNN Regressor", "SVM Regressor"]
+regression_model = StringVar()
+regression_model.set(regression_models[0])
+classifier_model = StringVar()
+classifier_model.set(classifier_models[0])
+
 
 ##################################Pre Processing Functions#############################################
 def check_null_drop(data):
     global logTable, lineNum
     dim = list(data.shape)
-    logTable.insert(lineNum, "Initial no. of rows : " + str(dim[0]))
+    logTable.insert(lineNum, "Initial no. of rows : " + str(dim[0]) + "\n")
     lineNum = lineNum + 1
     null_list = list(data.isnull().sum())
     null_list_name = list(data.isnull())
-    logTable.insert(lineNum, "The Columns having null values : ")
+    logTable.insert(lineNum, "The Columns having null values : \n")
     lineNum = lineNum + 1
     for i in range(len(null_list)):
         if null_list[i] != 0:
             temp = null_list_name[i]
             logTable.insert(lineNum, str(temp))
             lineNum = lineNum + 1
-    logTable.insert(lineNum, "Dropping the rows having null values!")
+    logTable.insert(lineNum, "Dropping the rows having null values!\n")
     lineNum = lineNum + 1
     data = data.dropna()
     data = data.reset_index(drop=True)
     dim = list(data.shape)
-    logTable.insert(lineNum, "No. of rows after dropping the NaN values : " + str(dim[0]))
+    logTable.insert(lineNum, "No. of rows after dropping the NaN values : " + str(dim[0] + "\n"))
     lineNum = lineNum + 1
     return data
 
@@ -70,7 +86,7 @@ def check_null_drop(data):
 
 def distribution_plot(data):
     global logTable, lineNum
-    logTable.insert(lineNum, "The Data Distribution is plotted and saved as .png file")
+    logTable.insert(lineNum, "The Data Distribution is plotted and saved as .png file\n")
     lineNum = lineNum + 1
     data.hist(figsize=(25,16))
     plt.savefig('images/dist_plot.png')
@@ -115,15 +131,15 @@ def skewness_check_removal(data):
             col_sqrt_temp.append(t2)
 
     if(len(col_sq_temp) < len(col_sqrt_temp)):
-        logTable.insert(lineNum, "Skewness of parameters reduced by squaring each value.")
+        logTable.insert(lineNum, "Skewness of parameters reduced by squaring each value.\n")
         lineNum = lineNum + 1
         return data_sq_rem
     elif(len(col_sqrt_temp) < len(col_sq_temp)):
-        logTable.insert(lineNum, "Skewness of parameters reduced by squareroot each value.")
+        logTable.insert(lineNum, "Skewness of parameters reduced by squareroot each value.\n")
         lineNum = lineNum + 1
         return data_sqrt_rem
     else:
-        logTable.insert(lineNum, "Skewness of parameters reduced by squaring each value.")
+        logTable.insert(lineNum, "Skewness of parameters reduced by squaring each value.\n")
         lineNum = lineNum + 1
         return data_sq_rem
 
@@ -138,14 +154,16 @@ def skewness_check_removal(data):
 
 
 def openFile():
-    global fileEntry, mainFrame, regressionSet, classifierSet, filename
+    global fileEntry, mainFrame, regressionSet, classifierSet, filename, logTable, lineNum
     filename = filedialog.askopenfilename(initialdir=".", title="Select your dataset", filetypes=(("Dataset", "*.csv"),))
     fileEntry['state'] = NORMAL
     fileEntry.delete(0, END)
     fileEntry.insert(0, str(filename))
+    logTable.insert(lineNum, "Selected file: " + str(filename) + "\n")
+    lineNum = lineNum + 1
     selectionLabel = Label(mainFrame, text="Choose type of model",anchor="nw").grid(row=2, column=0)
-    Radiobutton(mainFrame, text = "Regression", variable=modelType, value="regression", command=regressionSelection).grid(row=3, column=0, sticky=W)
-    Radiobutton(mainFrame, text = "Classifier", variable=modelType, value="classifier", command=classifierSelection).grid(row=4, column=0, sticky=W)
+    Radiobutton(mainFrame, text = "Regression", variable=regression_model, value="regression", command=regressionSelection).grid(row=3, column=0, sticky=W)
+    Radiobutton(mainFrame, text = "Classifier", variable=classifier_model, value="classifier", command=classifierSelection).grid(row=4, column=0, sticky=W)
     return
 
 explorerIcon = PhotoImage(file="iconFiles/file.png")
@@ -158,12 +176,6 @@ fileEntry.grid(row=1, column=0, columnspan=3, padx=3, pady=3)
 explorerButton.grid(row=1, column=3, padx=3, pady=3)
 
 
-classifier_models = ["SV Classifier", "KNN Classifier", "Deep Tree Classifier", "Random Forest Classifier"]
-regression_models = ["Decision Tree Regressor", "Random Forest Regressor", "KNN Regressor", "SVM Regressor"]
-regression_model = StringVar()
-regression_model.set(regression_models[0])
-classifier_model = StringVar()
-classifier_model.set(classifier_models[0])
 regressionLabel = Label(mainFrame, text="Choose type of regression model")
 regressionDrop = OptionMenu(mainFrame, regression_model, *regression_models)
 classifierLabel = Label(mainFrame,text="Choose type of classification model")
@@ -198,7 +210,12 @@ def regressionSelection():
     
 
 def start_proc():
-    global filename, regressionSet, classifierSet, lineNum
+    global filename, regressionSet, classifierSet, regression_model, classifier_model, logTable, lineNum
+    if (classifierSet):
+        logTable.insert(lineNum, "Processing Started with classifier model: " + str(classifier_model.get()) + "\n")
+    if (regressionSet):
+        logTable.insert(lineNum, "Processing Started with regression model: " + str(regression_model.get()) + "\n")
+    lineNum = lineNum + 1
     #data = pd.read_csv(filename)
     ##implement functions
     proc_Complete = True
