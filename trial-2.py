6287717453a7b36fbac3833f13b2f99ac8c9ae15
sumanta <sumanta>
2022-09-13 18:41:24 +0530

    WIP

--- trial-2.py
+++ trial-2.py
@@ -17,6 +17,20 @@ from tkinter import filedialog
 import os
 from os import listdir
 
+from sklearn.neighbors import KNeighborsRegressor, KNeighborsClassifier
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
+from sklearn.svm import SVR, SVC
+
+from scipy.stats import skew
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_recall_curve, average_precision_score,auc
+
+
 #initializing the main window
 mainWindow = Tk()
 mainWindow.title("MainWindow")
@@ -149,8 +163,127 @@ def skewness_check_removal(data):
 
  #######################################################################################################################################       
 
+#####################################Regression Functions#####################################################################
+
+def train_test_split_function(data_without_output, data_with_output):
+    real_x = data_without_output.values
+    real_y = data_with_output.iloc[:,-1].values
+
+    std_scl = StandardScaler()
+
+    real_x = std_scl.fit_transform(real_x)
+
+    x_train, x_test, y_train, y_test = train_test_split(real_x, real_y, test_size=0.15)
+
+    return(x_train, x_test, y_train, y_test)
 
 
+def DT_Regressor(x_train, x_test, y_train, y_test):
+    global lineNum, logTable
+    dtr = DecisionTreeRegressor()
+    dtr.fit(x_train, y_train)
+    accuracy = dtr.score(x_test,y_test)
+    y_predict_dtr = dtr.predict(x_test)
+    confusion_matrix_dtr = confusion_matrix(y_test,y_predict_dtr)
+
+    logTable.insert(lineNum, "Decision Tree Regressor : " + str(accuracy) + "\n")
+    lineNum = lineNum + 1 
+    print("\nConfusion Matrix : \n",confusion_matrix_dtr)
+
+
+def RF_Regressor(x_train, x_test, y_train, y_test):
+    rfr = RandomForestRegressor()
+    rfr.fit(x_train, y_train)
+    accuracy = rfr.score(x_test,y_test)
+    y_predict_rfr = rfr.predict(x_test)
+    confusion_matrix_rfr = confusion_matrix(y_test,y_predict_rfr)
+
+    print("Random Forest Regressor : ",accuracy)
+    print("\nConfusion Matrix : \n",confusion_matrix_rfr)
+
+
+def KNN_Regressor(x_train, x_test, y_train, y_test):
+    knn_reg = KNeighborsRegressor()
+    knn_reg.fit(x_train, y_train)
+    accuracy = knn_reg.score(x_test,y_test)
+    y_predict_knn_reg = knn_reg.predict(x_test)
+    confusion_matrix_knn_reg = confusion_matrix(y_test,y_predict_knn_reg)
+
+    print("K-Nearest Neighbour Regressor : ",accuracy)
+    print("\nConfusion Matrix : \n",confusion_matrix_knn_reg)
+
+
+def SVM_Regressor(x_train, x_test, y_train, y_test):
+    svm_reg = SVR()
+    svm_reg.fit(x_train, y_train)
+    accuracy = svm_reg.score(x_test,y_test)
+    y_predict_svm_reg = svm_reg.predict(x_test)
+    confusion_matrix_svm_reg = confusion_matrix(y_test,y_predict_svm_reg)
+
+    print("Support Vector Regressor : ",accuracy)
+    print("\nConfusion Matrix : \n",confusion_matrix_svm_reg)
+
+
+#########################################################################################################################################
+
+###############################Classification Functions##################################################################################
+
+def train_test_split_function(data_without_output, data_with_output):
+    real_x = data_without_output.values
+    real_y = data_with_output.iloc[:,-1].values
+
+    std_scl = StandardScaler()
+
+    real_x = std_scl.fit_transform(real_x)
+
+    x_train, x_test, y_train, y_test = train_test_split(real_x, real_y, test_size=0.15)
+
+    return(x_train, x_test, y_train, y_test)
+
+
+def sv_classifier(x_train,x_test,y_train,y_test):
+    linear_model=SVC(kernel='linear',C=1).fit(x_train,y_train)
+    predictor=linear_model.predict(x_test)
+    acc=linear_model.score(x_test,y_test)
+    confusion_matrix_svc = confusion_matrix(y_test,predictor)
+
+    print("Accuracy of the model=",acc*100,"%")
+    print("The confusion matrix: \n",confusion_matrix_svc)
+
+
+
+def knn_classifier(x_train, x_test, y_train, y_test):
+    knn_model=KNeighborsClassifier(n_neighbors=3).fit(x_train,y_train)
+    pred=knn_model.predict(x_test)
+    acc=knn_model.score(x_test,y_test)
+    confusion_matrix_knn = confusion_matrix(y_test,pred)
+
+    print("Accuracy of the model=",acc*100,"%")
+    print("The confusion matrix: \n",confusion_matrix_knn)
+
+
+def dtr_classifier(x_train, x_test, y_train, y_test):
+    model=DecisionTreeClassifier(criterion='entropy', max_depth=3).fit(x_train,y_train)
+    pred=model.predict(x_test)
+    # pred_arr=[y[i] for i in pred]
+    # print(pred_arr)
+    acc=model.score(x_test,y_test)
+    confusion_matrix_dtrc = confusion_matrix(y_test,pred)
+    print("Decision tree model accuracy=",acc*100)
+    print("The Confusion matrix: \n",confusion_matrix_dtrc)
+
+
+def rf_classifier(x_train, x_test, y_train, y_test):
+    rfc = RandomForestClassifier()
+    rfc.fit(x_train, y_train)
+    accuracy = rfc.score(x_test,y_test)
+    y_predict_rfc = rfc.predict(x_test)
+    confusion_matrix_rfc = confusion_matrix(y_test,y_predict_rfc)
+
+    print("Random Forest Classifier : ",accuracy)
+    print("\nConfusion Matrix : \n",confusion_matrix_rfc)
+
+#########################################################################################################################################
 
 
 def openFile():
